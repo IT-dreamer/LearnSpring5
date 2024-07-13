dd9363dd90287d15462bcb74cff26dfc641695e6
AFei <tjtgcyf@gmail.com>
2024-07-13 12:10:54 +0900

    first add

---
+++ src/main/java/com/starwiper/learnSpring5/LearnSpring5Application.java
@@ -0,0 +1,13 @@
+package com.starwiper.learnSpring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpring5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/starwiper/learnSpring5/LearnSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwiper.learnSpring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
